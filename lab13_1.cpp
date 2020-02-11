4af80e78022c42d2aa11167cb2322b075b3c3963
PossiblePack <possible.pack@gmail.com>
2020-02-11 23:32:22 +0700

    finished

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -11,10 +11,28 @@ void swap(T d[],int x,int y){
 
 template <typename T>
 void insertionSort(T d[],int N){
-	
-//Write your code here.
-
+	//Write your code here.
+	int position=0;
+	for(int j=1; j<N; j++){
+		for(int i=0; i<N; i++){
+			if(i!=j){
+				cout << " " << d[i] << " ";
+			}else cout << "[" << d[i] << "]";
+		}
+		cout << " =>  ";
+		for(int k=j; k>0 ;k--){
+			if(d[k]>d[k-1]) swap(d,k,k-1);
+			position[] = k;
+			}
+			for(int m=0; m<N; m++){
+				if(d[m]!=position[m]) 
+				cout << " " << d[m] << " ";
+				else cout << "[" << d[m] << "]";
+			}
+			cout << "\n";
+		}
 }
+
 
 int main(){
 	int a[10] = {12,25,30,44,2,0,4,7,55,25};
--- lab13_2.cpp
+++ lab13_2.cpp
@@ -21,3 +21,47 @@ int main(){
 }
 
 // Write definition of inputMatrix(),matrixMultiply() and showMatrix() here
+
+void inputMatrix(double A[0][N]){
+	for(int i=0;i<N;i++){
+		cout << "Row " << i+1 << ": ";
+		for(int j=0;j<N;j++){
+			cin >> A[i][j];
+		}
+	}
+}
+
+void findLocalMax(const double A[][N], bool B[][N]){
+	for(int i=0;i<=3;i++){
+		for(int j=0;j<=3;j++){
+			if(A[i][j]>=A[i][j-1]&&A[i][j]>=A[i][j+1]&&A[i][j]>=A[i-1][j]&&A[i][j]>=A[i+1][j]){
+				B[i][j] = 1;
+				B[i][j+1] = 0;
+				B[i][j-1] = 0;
+				B[i+1][j] = 0;
+				B[i-1][j] = 0;
+			}else{
+				B[i][j] = 0;
+			}
+		}
+	}
+	for(int i=0;i<N;i++){
+		for(int j=0;j<N;j+=N-1){
+			B[i][j] = 0;
+		}
+	}
+	for(int i=0;i<N;i+=N-1){
+		for(int j=0;j<N;j++){
+			B[i][j] = 0;
+		}
+	}
+}
+
+void showMatrix(const bool M[][N]){
+	for(int k=0;k<N;k++){
+		for(int l=0;l<N;l++){
+			cout << M[k][l] << " ";
+		}
+		cout << "\n";
+	}
+}
